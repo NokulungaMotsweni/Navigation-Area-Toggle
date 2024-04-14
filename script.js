f52df6739a2ee2d88906b4e753cf53e630d9dbfb
Nokulunga Motsweni <nokulunga.motsweni@praguecollege.ca>
2024-04-14 21:35:16 +0200

    script.js changes

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-    function toggleNavigation() {
+function toggleIcons() {
+    var menuIcon = document.getElementById("menu");
+    var closeIcon = document.getElementById("close");
+
+    menuIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+}
+function toggleNavigation() {
         var navElement = document.querySelector("nav");
         navElement.classList.toggle("shownav");
+        toggleIcons();
     }
     
     // Attach click event listeners to menu and close icons
     document.getElementById("menu").addEventListener("click", toggleNavigation);
-    document.getElementById("close").addEventListener("click", toggleNavigation);
-    
-
-document.getElementById("menu").addEventListener("click", toggleNav);
-document.getElementById("close").addEventListener("click", toggleNav);
+    document.getElementById("close").addEventListener("click", toggleNavigation);    
